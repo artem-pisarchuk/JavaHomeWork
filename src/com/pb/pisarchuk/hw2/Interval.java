9ac0c179e026b0902f8469b0c0c7af9eb117ce2e
Artem Pisarchuk <pisarchuk.artem.pb@gmail.com>
2021-10-15 18:13:06 +0300

    init commit

---
+++ src/com/pb/pisarchuk/hw2/Interval.java
@@ -0,0 +1,22 @@
+package com.pb.pisarchuk.hw2;
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        System.out.println("Введите число:");
+        Scanner input = new Scanner(System.in);
+        int number;
+        number = input.nextInt();
+        if (number>=0 && number<=14){
+            System.out.println (number + " находится в промежутке [0..14]");
+        } else if (number>=15 && number<=35){
+            System.out.println (number + " находится в промежутке [15..35]");
+        } else if (number>=36 && number<=50) {
+            System.out.println(number + " находится в промежутке [36..50]");
+        } else if (number>=51 && number<=100) {
+            System.out.println(number + " находится в промежутке [51..100]");
+        } else {
+            System.out.println(number + " не попало ни в один промежуток");
+        }
+    }
+}
